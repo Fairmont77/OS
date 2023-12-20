5a8d76474f03e316bba450c4bb549848dcea32fd
Casper <91886585+Fairmont77@users.noreply.github.com>
2023-12-20 20:08:58 +0200

    added comments

--- OS_Lab1/src/main/java/Functions/Functions.java
+++ OS_Lab1/src/main/java/Functions/Functions.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 
 public class Functions {
 
+    // calculating sqrt(x). Result if x < x = empty
     public static Optional<Double> functionF(Integer x) {
         return x > 0 ? Optional.of(Math.sqrt(x)) : Optional.empty();
     }
+    //calc factorial(x). Result if x < 0 = empty
     public static Optional<Double> functionG(Integer x) {
         if (x < 0) {
             return Optional.empty();
