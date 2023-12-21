bcbc482d6acf01adac06de06d31b41184c14d7e5
Casper <91886585+Fairmont77@users.noreply.github.com>
2023-12-21 17:42:56 +0200

    Add SJN algorithm

--- OS_Lab2_moss/src/main/java/org/example/SchedulingAlgorithm.java
+++ OS_Lab2_moss/src/main/java/org/example/SchedulingAlgorithm.java
@@ -1,70 +1,66 @@
-package org.example;// Run() is called from Scheduling.main() and is where
+// Run() is called from Scheduling.main() and is where
 // the scheduling algorithm written by the user resides.
 // User modification should occur within the Run() function.
+package org.example;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.Vector;
 import java.io.*;
 
 public class SchedulingAlgorithm {
 
-  public static Results Run(int runtime, Vector processVector, Results result) {
-    int i = 0;
+  public static Results Run(int runtime, Vector<sProcess> processVector, Results result) {
     int comptime = 0;
-    int currentProcess = 0;
-    int previousProcess = 0;
-    int size = processVector.size();
-    int completed = 0;
+    sProcess currentProcess;
+
+    PriorityQueue<sProcess> queue = new PriorityQueue<>(Comparator.comparingInt(sProcess::getCputime));
+    queue.addAll(processVector);
+
     String resultsFile = "Summary-Processes";
 
-    result.schedulingType = "Batch (Nonpreemptive)";
-    result.schedulingName = "First-Come First-Served"; 
     try {
-      //BufferedWriter out = new BufferedWriter(new FileWriter(resultsFile));
-      //OutputStream out = new FileOutputStream(resultsFile);
       PrintStream out = new PrintStream(new FileOutputStream(resultsFile));
-      sProcess process = (sProcess) processVector.elementAt(currentProcess);
-      out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
-      while (comptime < runtime) {
-        if (process.cpudone == process.cputime) {
-          completed++;
-          out.println("Process: " + currentProcess + " completed... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
-          if (completed == size) {
-            result.compuTime = comptime;
-            out.close();
-            return result;
-          }
-          for (i = size - 1; i >= 0; i--) {
-            process = (sProcess) processVector.elementAt(i);
-            if (process.cpudone < process.cputime) { 
-              currentProcess = i;
-            }
-          }
-          process = (sProcess) processVector.elementAt(currentProcess);
-          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
-        }      
-        if (process.ioblocking == process.ionext) {
-          out.println("Process: " + currentProcess + " I/O blocked... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
-          process.numblocked++;
-          process.ionext = 0; 
-          previousProcess = currentProcess;
-          for (i = size - 1; i >= 0; i--) {
-            process = (sProcess) processVector.elementAt(i);
-            if (process.cpudone < process.cputime && previousProcess != i) { 
-              currentProcess = i;
-            }
-          }
-          process = (sProcess) processVector.elementAt(currentProcess);
-          out.println("Process: " + currentProcess + " registered... (" + process.cputime + " " + process.ioblocking + " " + process.cpudone + " " + process.cpudone + ")");
-        }        
-        process.cpudone++;       
-        if (process.ioblocking > 0) {
-          process.ionext++;
+
+      while (comptime < runtime && !queue.isEmpty()) {
+        currentProcess = queue.poll(); // Вибір процесу з найкоротшим часом виконання
+
+        // Записуємо, що процес зареєстровано
+        out.println("Process: " + currentProcess.hashCode() + " registered... (" + currentProcess.getCputime() + " " + currentProcess.getIoblocking() + " " + currentProcess.getCpudone() + " " + currentProcess.getNumblocked() + ")");
+
+        // Виконання поточного процесу
+        while (currentProcess.getCpudone() < currentProcess.getCputime() && comptime < runtime) {
+          currentProcess.setCpudone(currentProcess.getCpudone() + 1);
+          comptime++;
         }
-        comptime++;
+
+        // Записуємо, що процес завершився
+        out.println("Process: " + currentProcess.hashCode() + " completed. CPU Time: " + currentProcess.getCputime() + " ms, CPU Done: " + currentProcess.getCpudone() + " ms");
       }
+
       out.close();
-    } catch (IOException e) { /* Handle exceptions */ }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
     result.compuTime = comptime;
     return result;
   }
 }
+
+  /* private static Vector<sProcess> setPriority(Vector<sProcess> processes) {
+    int priority= 0;
+    int prevTime = 0;
+    processes.sort(Comparator.comparing(sProcess::getCputime).thenComparing(sProcess::getIoblocking));
+
+    for (sProcess process : processes) {
+      if(prevTime < process.getCputime()) {
+        priority += 1;
+      }
+      prevTime = process.getCputime();
+      process.setPriority(priority);
+    }
+    return processes;
+  }
+}*/
